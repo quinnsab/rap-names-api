c06ca54d7fd307ef5b7944dc51aff041598132d5
Erin Sheridan <erinsabs@gmail.com>
2022-09-27 10:37:28 -0400

    working rapper api

---
+++ server.js
@@ -0,0 +1,46 @@
+const { request, response } = require('express')
+const express = require('express')
+const app = express()  //the app can now use all the things that come with express
+const PORT = 8000
+
+const rappers = {
+   '21 savage': {
+    'age': 29,
+    'birthName': 'Sheyaa Bin Abraham-Joseph',
+    'birthLocation': 'London, England'
+    },
+    
+    'chance the rapper': {
+        'age': 29,
+        'birthName': 'Chancelor Bennett',
+        'birthLocation': 'Chicago, Illinois'
+    },
+
+    'unknown': {
+        'age': '0',
+        'birthName': 'unknown',
+        'birthLocation': 'unknown'
+    }
+    
+}
+
+//this is essentially an event listener, but instead of it hearing a click, its a network request
+app.get('/', (request, response) => {  
+    response.sendFile(__dirname + '/index.html')
+
+})
+
+//respond with an object, but send it back as json
+//  ( rappers[rapperName] ).birthName   We are grabbing the object & we can use any of the properties inside that object
+app.get('/api/:name', (request, response) => {
+    const rapperName = request.params.name.toLowerCase()
+        if(rappers[rapperName]){
+            response.json(rappers[rapperName])
+        }else{
+            response.json(rappers['unknown'])
+        }
+})
+
+app.listen(PORT, () => {
+    console.log(`The server is running on port ${PORT}! You better catch it!`)
+})
